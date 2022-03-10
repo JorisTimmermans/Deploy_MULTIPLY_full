ac3af96de942500e7854e17086d3a14f54206819
Joris Timmermans <j.timmermans@gmail.com>
2022-03-10 16:43:58 +0100

    Changed where to get the modified modules
    I modified several modules to allow MULTIPLY to run on more arbitrary systems. Initially these modules were extracted from the zip files attached in the archive folder. This is now changed to purely download these files from their respective forks on github/JorisTimmermans

--- install_download_multiply_modules.sh
+++ install_download_multiply_modules.sh
@@ -9,30 +9,29 @@
 mkdir $software_dir
 
 ################################
-#### Use the local package-repository
+#### Get all original modules from from github
 ################################
-# unzip archive/multiply_modules_max.zip -d $software_dir 
+# git clone https://github.com/multiply-org/inference-engine.git $software_dir'/inference_engine'
+# git clone https://github.com/multiply-org/KaFKA-InferenceEngine.git $software_dir'/kafka'
+# git clone https://github.com/multiply-org/kaska.git $software_dir'/kaska'
+# git clone https://github.com/multiply-org/prior-engine.git $software_dir'/multiply_prior_engine'
+# git clone https://github.com/multiply-org/multiply-ui.git $software_dir'/multiply_ui'
+# git clone https://github.com/multiply-org/MULTIPLYVisualisation.git $software_dir'/MULTIPLYVisualisation'
+# git clone https://github.com/multiply-org/sar-pre-processing.git  $software_dir'/sar_pre-processing'
 
 ################################
-#### Alternatively, download the majority from github
-####  please note some of the package's were modified from their github variants to get 
+####  please note some of the package's were modified from their original multiply.org versions to get 
 ####  the system to work. Hence, these are within the local  to be installed from the local package-repo
 ################################
-# unzip multiply_modules_min.zip -d $software_dir
 
-unzip multiply_modules_min1.zip -d $software_dir
-unzip multiply_modules_min2.zip -d $software_dir
-unzip multiply_modules_min3.zip -d $software_dir
-unzip multiply_modules_min4.zip -d $software_dir
+# git clone https://github.com/JorisTimmermans/atmospheric_correction.git $software_dir'/atmospheric_correction'
+# git clone https://github.com/JorisTimmermans/BRDF_descriptors.git $software_dir'/BRDF_descriptors'
+# git clone https://github.com/JorisTimmermans/data-access.git $software_dir'/data-access'
+# git clone https://github.com/JorisTimmermans/multiply-core.git $software_dir'/multiply-core'
+# git clone https://github.com/JorisTimmermans/multiply-ui.git $software_dir'/multiply-ui'
+# git clone https://github.com/JorisTimmermans/vm-support.git $software_dir'/vm-support-ui'
 
-git clone https://github.com/multiply-org/atmospheric_correction.git $software_dir'/atmospheric_correction'
-git clone https://github.com/multiply-org/inference-engine.git $software_dir'/inference_engine'
-git clone https://github.com/multiply-org/KaFKA-InferenceEngine.git $software_dir'/kafka'
-git clone https://github.com/multiply-org/kaska.git $software_dir'/kaska'
-git clone https://github.com/multiply-org/prior-engine.git $software_dir'/multiply_prior_engine'
-git clone https://github.com/multiply-org/multiply-ui.git $software_dir'/multiply_ui'
-git clone https://github.com/multiply-org/MULTIPLYVisualisation.git $software_dir'/MULTIPLYVisualisation'
-git clone https://github.com/multiply-org/sar-pre-processing.git  $software_dir'/sar_pre-processing'
-
-# not yet in the repository, but with github
-git clone https://github.com/bcdev/calvalus-instances $software_dir'/calvalus-instances'
+################################
+####  external modules
+################################
+# git clone https://github.com/bcdev/calvalus-instances $software_dir'/calvalus-instances'
